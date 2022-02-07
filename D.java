1e2c6db5f724ad0c6f69fe5e42c8f5b6084035b6
giovannatrindade <giovannacstrindade@gmail.com>
2022-02-06 22:30:25 -0300

    Criação da classe D

---
+++ D.java
@@ -0,0 +1,38 @@
+public class D{
+    //atributos
+    private String D1;
+    private int D2;
+
+    //construtor
+    public D(String D1, int D2){
+        this.D1 = D1;
+        this.D2 = D2;
+    }
+
+    //métodos
+    public String getD1() {
+        return D1;
+    }
+
+    public int getD2(){
+        return D2;
+    }
+
+    public void setD1(String D1) {
+        this.D1 = D1;
+    }
+
+    public void setD2(int D2){
+        this.D2 = D2;
+    }
+
+    public String MD1(){
+        System.out.println("MD1");
+        return "MD1";
+    }
+
+    public String MD2(){
+        System.out.println("MD2");
+        return "MD2";
+    }
+}
