baec4eb781dc3b134e129d8a88fc0ea3663dbe55
Giovanna Trindade <89998661+giovannatrindade@users.noreply.github.com>
2022-02-06 23:04:07 -0300

    Update D.java

--- D.java
+++ D.java
@@ -35,4 +35,9 @@ public class D{
         System.out.println("MD2");
         return "MD2";
     }
+    
+    public String MD4(){
+        System.out.println("MD4");
+        return "MD4";
+    }
 }
